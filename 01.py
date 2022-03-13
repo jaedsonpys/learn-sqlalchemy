1a774f4f43896a62229bb21792a69d7b36e30419
Jaedson Silva <contacanal798@gmail.com>
2022-03-13 11:46:36 -0300

    using query.filter()

--- 01.py
+++ 01.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import text
 
 # create engine
 engine = create_engine('sqlite:///test.db')
@@ -48,5 +49,10 @@ for row in all_users:
 
 
 # using filter
-result = session.query(Users).filter('age > 18')
-print(result.name)
+result = session.query(Users).filter(Users.age > 18)
+
+print('\nFiltred Users:')
+for row in result:
+    print(f'ID: {row.id}')
+    print(f'Name: {row.name}')
+    print(f'Age: {row.age}')
